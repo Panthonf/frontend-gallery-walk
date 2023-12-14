2d5c49db0f11278864ac76b5d058d1b502f2a01d
Panthonf <panthon_kansap@cmu.ac.th>
2023-12-15 00:00:57 +0700

    Create PrivateRoutes.tsx

---
+++ src/PrivateRoutes.tsx
@@ -0,0 +1,26 @@
+import { Outlet, Navigate } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const PrivateRoutes = () => {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/isLoggedIn", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log("ddd", res.data.authenticated);
+        setToken(res.data.authenticated);
+      })
+      .catch((err) => {
+        setToken(false);
+        console.log("eee", err);
+      });
+  }, []); // Empty dependency array ensures useEffect runs only once when the component mounts
+
+  return token === null ? null : token ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRoutes;
