7bdeca58bc2dee2bfe348c5d666f6d9ccea5a4e7
NuttawanR <reabreang.nuttawan@gmail.com>
2023-12-20 16:07:25 +0700

    fix: bug private route

--- src/PrivateRoutes.tsx
+++ src/PrivateRoutes.tsx
@@ -1,16 +1,21 @@
 import { Outlet, Navigate } from "react-router-dom";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { useEffect, useState } from "react";
 
+interface AuthResponse {
+  authenticated: boolean;
+  // Add other properties if present in your response
+}
+
 const PrivateRoutes = () => {
-  const [token, setToken] = useState<any>(null);
+  const [token, setToken] = useState<boolean | null>(null);
 
   useEffect(() => {
     axios
-      .get("http://localhost:8080/isLoggedIn", {
+      .get<AuthResponse>(import.meta.env.VITE_CHECK_LOGIN, {
         withCredentials: true,
       })
-      .then((res) => {
+      .then((res: AxiosResponse<AuthResponse>) => {
         console.log("ddd", res.data.authenticated);
         setToken(res.data.authenticated);
       })
@@ -18,7 +23,7 @@ const PrivateRoutes = () => {
         setToken(false);
         console.log("eee", err);
       });
-  }, []); // Empty dependency array ensures useEffect runs only once when the component mounts
+  }, []);
 
   return token === null ? null : token ? <Outlet /> : <Navigate to="/login" />;
 };
