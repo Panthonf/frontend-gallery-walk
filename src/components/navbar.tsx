a264eb56bd873fc261d02d0efd5cebafb9eb7e98
NuttawanR <reabreang.nuttawan@gmail.com>
2023-12-20 16:06:17 +0700

    add: component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,51 +1,65 @@
 import { useState } from 'react';
-import { Center, Tooltip, UnstyledButton, Stack, rem } from '@mantine/core';
+import { NavLink } from 'react-router-dom';
+import { Center, Tooltip, UnstyledButton, Stack, rem, Container } from '@mantine/core';
 import {
     IconLayoutDashboard,
+    IconSquarePlus,
 
 } from '@tabler/icons-react';
 
 import styles from "./styles.module.css";
 
 interface NavbarLinkProps {
-  icon: typeof IconLayoutDashboard;
-  label: string;
-  active?: boolean;
-  onClick?(): void;
+    icon: typeof IconLayoutDashboard;
+    label: string;
+    active?: boolean;
+    onClick?(): void;
+}
+
+interface NavbarProps {
+    activeIndex: number;
 }
 
 function NavbarLink({ icon: Icon, label, active, onClick }: NavbarLinkProps) {
-  return (
-    <Tooltip label={label} position="right" transitionProps={{ duration: 0 }}>
-      <UnstyledButton onClick={onClick} className={styles.link} data-active={active || undefined}>
-        <Icon style={{ width: rem(20), height: rem(20) }} stroke={1.5} />
-      </UnstyledButton>
-    </Tooltip>
-  );
+    return (
+        <Tooltip label={label} position="right" transitionProps={{ duration: 0 }}>
+            <UnstyledButton onClick={onClick} className={styles.link} data-active={active || undefined}>
+                <Icon style={{ width: rem(20), height: rem(20), padding: rem(1) }} stroke={1.5} />
+            </UnstyledButton>
+        </Tooltip>
+    );
 }
 
 const mockdata = [
-  { icon: IconLayoutDashboard, label: 'Dashboard' },
+    { icon: IconLayoutDashboard, label: 'Dashboard' },
+    { icon: IconSquarePlus, label: 'Create-Event' },
 ];
 
-export default function Navbar() {
+export default function Navbar({ activeIndex }: NavbarProps) {
 
-    const [active, setActive] = useState(2);
+    const [active, setActive] = useState(0);
 
     const links = mockdata.map((link, index) => (
-        <NavbarLink
-            {...link}
+        <NavLink
+            to={`/${link.label.toLowerCase()}`}
             key={link.label}
-            active={index === active}
-            onClick={() => setActive(index)}
-        />
+            className={styles.link}
+        >
+            <NavbarLink
+                {...link}
+                active={index === activeIndex}
+                onClick={() => setActive(index)}
+            />
+        </NavLink>
     ));
 
     return (
-        <body >
-            <nav className={styles.navbar}>
+        <body>
+            <Container p="xl" style={{
+                height: "100vh",
+            }}>
                 <Center>
-                    
+
                 </Center>
 
                 <div className={styles.navbarMain}>
@@ -54,12 +68,28 @@ export default function Navbar() {
                     </Stack>
                 </div>
 
-                {/* <Stack justify="center" gap={0}>
-                    <NavbarLink icon={IconSwitchHorizontal} label="Change account" />
-                    <NavbarLink icon={IconLogout} label="Logout" />
-                </Stack> */}
-            </nav>
+
+            </Container>
         </body>
     );
 
 }
+
+{/* <div className={styles.navbarMain}>
+                    <Stack justify="center" gap={0}>
+                        {links}
+                    </Stack>
+                </div> */}
+{/* <nav className={styles.navbar}>
+                    <Center>
+                    </Center>
+                    <div className={styles.navbarMain}>
+                        <Stack justify="center" gap={0}>
+                            {links}
+                        </Stack>
+                    </div>
+                    <Stack justify="center" gap={0}>
+                        <NavbarLink icon={IconSwitchHorizontal} label="Change account" />
+                        <NavbarLink icon={IconLogout} label="Logout" />
+                    </Stack>
+                </nav> */}
