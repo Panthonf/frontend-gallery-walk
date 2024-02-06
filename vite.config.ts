650457a67727be4267dd92419d2f193cbad5dc72
Panthonf <panthon_kansap@cmu.ac.th>
2024-02-06 17:08:14 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/", // You might need to adjust this depending on your project's setup
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ["react"],
-          "react-dom": ["react-dom"],
+export default defineConfig(({ command }) => {
+  const isProduction = command === 'build';
+  return {
+    plugins: [react()],
+    base: "/", // You might need to adjust this depending on your project's setup
+    build: {
+      outDir: isProduction ? 'production-build' : 'build',
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            react: ["react"],
+            "react-dom": ["react-dom"],
+          },
+        },
+        input: {
+          main: "index.html", // Path to your index.html file
         },
       },
-      input: {
-        main: "index.html", // Path to your index.html file
-      },
     },
-  },
+  };
 });
