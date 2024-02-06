eabf28dec7a11027cf9258a1373414a717986dca
Panthonf <panthon_kansap@cmu.ac.th>
2024-02-06 17:24:33 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig(() => {
-  return {
-    plugins: [react()],
-    base: "/",
-    build: {
-      outDir: "dist",
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            react: ["react"],
-            "react-dom": ["react-dom"],
-          },
-        },
-        input: {
-          main: "index.html",
-        },
-      },
-    },
-  };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
