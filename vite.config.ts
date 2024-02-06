02086b1030800e69f10abb1739313f149b8cae3b
Panthonf <panthon_kansap@cmu.ac.th>
2024-02-06 17:04:39 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,23 +5,16 @@ export default defineConfig({
   plugins: [react()],
   base: "/", // You might need to adjust this depending on your project's setup
   build: {
-    outDir: "dist",
-    emptyOutDir: true, // Ensure the output directory is empty before building
-    lib: {
-      entry: "src/main.tsx",
-      name: "react-vite",
-      fileName: "index",
-    },
     rollupOptions: {
-      external: ["react", "react-router", "react-router-dom", "react-redux"],
       output: {
-        globals: {
-          react: "React",
+        manualChunks: {
+          react: ["react"],
+          "react-dom": ["react-dom"],
         },
       },
+      input: {
+        main: "index.html", // Path to your index.html file
+      },
     },
-  },
-  server: {
-    port: 3000, // Adjust the port if necessary
   },
 });
