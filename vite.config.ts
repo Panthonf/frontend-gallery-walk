19e05bfee2a210d502d01816d04d2c0f4702c32b
Panthonf <panthon_kansap@cmu.ac.th>
2024-02-06 16:45:55 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: "/", // You might need to adjust this depending on your project's setup
+  build: {
+    outDir: "dist",
+    emptyOutDir: true, // Ensure the output directory is empty before building
+    lib: {
+      entry: "src/index.ts",
+      name: "react-vite",
+      fileName: (format) => `react-vite.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-router", "react-router-dom", "react-redux"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000, // Adjust the port if necessary
+  },
 });
