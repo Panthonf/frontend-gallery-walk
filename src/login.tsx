6da61b144c122a04bde361c380a541f905dd1e60
Panthonf <panthon_kansap@cmu.ac.th>
2024-02-15 20:30:41 +0700

    Update login.tsx

--- src/login.tsx
+++ src/login.tsx
@@ -1,10 +1,4 @@
-import {
-    Text,
-    Center,
-    Container,
-    Button,
-    Divider,
-} from "@mantine/core";
+import { Text, Center, Container, Button } from "@mantine/core";
 
 import "@mantine/core/styles.css";
 
@@ -12,59 +6,51 @@ import "@mantine/core/styles.css";
 import styles from "./styles.module.css";
 
 import {
-    IconBrandGoogleFilled,
-    IconBrandFacebookFilled,
+  IconBrandGoogleFilled,
 } from "@tabler/icons-react";
 
 export default function Login() {
+  // google login
+  const handleGoogleLogin = () => {
+    window.location.href = import.meta.env.VITE_BACKEND_ENDPOINT;
+  };
 
-    // google login
-    const handleGoogleLogin = () => {
-        window.location.href = import.meta.env.VITE_BACKEND_ENDPOINT;
-    };
-
-    return (
-        <body>
-
-            <Center style={{
-                height: "100vh",
-            }}>
-                <Container size="xs" p="3rem" className={styles.logincontainer}>
-                    <Text size="topic" c="redcolor.4" fw={500}>
-                        Let's get Started!
-                    </Text>
-
-                    <Text mt="lg">
-                        Lorem ipsum dolor sit amet consectetur adipisicing
-                        elit. Quasi at amet perspiciatis in, quas nobis, sit
-                        id blanditiis recusandae saepe atque laudantium
-                        ratione asperiores culpa numquam provident,
-                        obcaecati repellat? Autem!
-                    </Text>
-                    <Center>
-                        <Button
-                            color="deepredcolor.9"
-                            size="md"
-                            w="100%"
-                            justify="center"
-                            variant="outline"
-                            mt="xl"
-                            leftSection={
-                                <IconBrandGoogleFilled size={14} />
-                            }
-                            onClick={handleGoogleLogin}>
-                            <Text c="deepredcolor.9">
-                                Login with Google account
-                            </Text>
-                        </Button>
-                    </Center>
-                    <Divider
+  return (
+    <body>
+      <Center
+        style={{
+          height: "100vh",
+        }}
+      >
+        <Container size="xs" p="3rem" className={styles.logincontainer}>
+          <Text size="topic" c="redcolor.4" fw={500}>
+            Let's get Started!
+          </Text>
+          <Text mt="lg">
+            Welcome back, members! Please log in to continue in Event
+            Management.
+          </Text>
+          <Center>
+            <Button
+              color="deepredcolor.9"
+              size="md"
+              w="100%"
+              justify="center"
+              variant="outline"
+              mt="xl"
+              leftSection={<IconBrandGoogleFilled size={14} />}
+              onClick={handleGoogleLogin}
+            >
+              <Text c="deepredcolor.9">Login with Google account</Text>
+            </Button>
+          </Center>
+          {/* <Divider
                         my="lg"
                         label="or"
                         c="graycolor.2"
                         labelPosition="center"
-                    />
-                    <Center>
+                    /> */}
+          {/* <Center>
                         <Button
                             color="deepredcolor.9"
                             size="md"
@@ -80,10 +66,9 @@ export default function Login() {
                                 Login with Facebook account
                             </Text>
                         </Button>
-                    </Center>
-                </Container>
-
-            </Center>
-        </body>
-    );
+                    </Center> */}
+        </Container>
+      </Center>
+    </body>
+  );
 }
