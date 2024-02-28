9aef51e4e4924f295889951f963cc22963222d2a
Panthonf <panthon_kansap@cmu.ac.th>
2024-02-28 17:40:06 +0700

    Merge branch 'dev-kansap' into dev-nuttawan

--- src/Guest/guestEventDashboard.tsx
+++ src/Guest/guestEventDashboard.tsx
@@ -2,18 +2,24 @@ import { useEffect, useState } from "react";
 
 import axios from "axios";
 import {
-    Flex,
-    Text,
-    Grid,
-    ActionIcon,
-    Box,
-    Modal,
-    Badge,
-    AspectRatio,
-    Image,
-    Group,
-    Paper,
-
+  Anchor,
+  Card,
+  // Center,
+  Collapse,
+  Flex,
+  // Progress,
+  Text,
+  Image,
+  Divider,
+  Grid,
+  AspectRatio,
+  ActionIcon,
+  Box,
+  Group,
+  UnstyledButton,
+  Modal,
+  Title,
+  Badge,
 } from "@mantine/core";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import styles from "../styles.module.css";
@@ -22,267 +28,266 @@ import moment from "moment";
 import { useDisclosure } from "@mantine/hooks";
 import { LoadingIndicator } from "../components/loading";
 import {
-    IconArrowsDiagonal,
-    IconCoins,
+  IconArrowsDiagonal,
+  IconChevronDown,
+  IconChevronUp,
 } from "@tabler/icons-react";
+// import { relative } from "path";
 
 type EventType = {
-    id: number;
-    start_date: string;
-    end_date: string;
-    event_name: string;
-    virtual_money: number;
-    unit_money: string;
-    description: string;
-    video_link: string;
+  id: number;
+  start_date: string;
+  end_date: string;
+  event_name: string;
+  virtual_money: number;
+  unit_money: string;
+  description: string;
+  video_link: string;
 };
 
 type GuestType = {
-    id: number;
-    first_name_en: string;
-    last_name_en: string;
-    email: string;
-    virtual_money: number;
+  id: number;
+  first_name_en: string;
+  last_name_en: string;
+  email: string;
+  virtual_money: number;
 };
 export default function GuestEventDashboard() {
-    const { eventId } = useParams();
-    console.log("event id", eventId);
-    const guestId = useLocation().search.split("=")[1];
-    const navigate = useNavigate();
-    const [eventData, setEventData] = useState<EventType | null>(null);
-    const [thumbnailUrl, setThumbnailUrl] = useState<string>(
-        `https://placehold.co/400?text${eventId}`
-    );
-    // const [opened, { toggle }] = useDisclosure(false);
-    const [isLoading, setIsLoading] = useState(true);
-    const [guestData, setGuestData] = useState<GuestType | null>(null);
-    // const [opened, { open, close }] = useDisclosure(false);
-    // const [isOpened, { toggle }] = useDisclosure(false);
+  const { eventId } = useParams();
+  console.log("event id", eventId);
+  const guestId = useLocation().search.split("=")[1];
+  const navigate = useNavigate();
+  const [eventData, setEventData] = useState<EventType | null>(null);
+  const [thumbnailUrl, setThumbnailUrl] = useState<string>(
+    `https://placehold.co/400?text${eventId}`
+  );
+  // const [opened, { toggle }] = useDisclosure(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [guestData, setGuestData] = useState<GuestType | null>(null);
+  const [opened, { open, close }] = useDisclosure(false);
+  const [isOpened, { toggle }] = useDisclosure(false);
 
-    useEffect(() => {
-        const fetchEventData = async () => {
-            try {
-                await axios
-                    .get(
-                        `${import.meta.env.VITE_BASE_ENDPOINTMENT}guests/events/${eventId}`,
-                        {
-                            withCredentials: true,
-                        }
-                    )
-                    .then((res) => {
-                        console.log("event data", res.data.data);
-                        document.title = `${res.data.data.event_name}`;
-                        setEventData(res.data.data);
-                        setIsLoading(false);
-                    })
-                    .catch((err) => {
-                        console.log("err", err.response.data.success);
-                        navigate("/404");
-                    });
-            } catch (error) {
-                console.log("err", error);
-                navigate("/404");
+  useEffect(() => {
+    const fetchEventData = async () => {
+      try {
+        await axios
+          .get(
+            `${import.meta.env.VITE_BASE_ENDPOINTMENT}guests/events/${eventId}`,
+            {
+              withCredentials: true,
             }
-        };
-        const fetchThumbnail = async () => {
-            try {
-                await axios
-                    .get(
-                        `${import.meta.env.VITE_BASE_ENDPOINTMENT
-                        }events/thumbnail/${eventId}`,
-                        {
-                            withCredentials: true,
-                        }
-                    )
-                    .then((res) => {
-                        console.log("fetch thumbnail", res.data.data[0].thumbnail_url);
-                        setThumbnailUrl(res.data.data[0].thumbnail_url);
-                    })
-                    .catch(() => {
-                        // console.log("err", err);
-                    });
-            } catch (err) {
-                // console.log("err", err);
+          )
+          .then((res) => {
+            console.log("event data", res.data.data);
+            document.title = `${res.data.data.event_name}`;
+            setEventData(res.data.data);
+            setIsLoading(false);
+          })
+          .catch((err) => {
+            console.log("err", err.response.data.success);
+            navigate("/404");
+          });
+      } catch (error) {
+        console.log("err", error);
+        navigate("/404");
+      }
+    };
+    const fetchThumbnail = async () => {
+      try {
+        await axios
+          .get(
+            `${
+              import.meta.env.VITE_BASE_ENDPOINTMENT
+            }events/thumbnail/${eventId}`,
+            {
+              withCredentials: true,
             }
-        };
+          )
+          .then((res) => {
+            console.log("fetch thumbnail", res.data.data[0].thumbnail_url);
+            setThumbnailUrl(res.data.data[0].thumbnail_url);
+          })
+          .catch(() => {
+            // console.log("err", err);
+          });
+      } catch (err) {
+        // console.log("err", err);
+      }
+    };
 
-        const checkGuestSession = async () => {
-            try {
-                await axios
-                    .get(
-                        `${import.meta.env.VITE_BASE_ENDPOINTMENT
-                        }guests/check-guest-session?eventId=${eventId}&guestId=${guestId}`,
-                        {
-                            withCredentials: true,
-                        }
-                    )
-                    .then((res) => {
-                        if (res.data.success === false) {
-                            navigate("/guest/login");
-                        }
-                    })
-                    .catch((err) => {
-                        console.log("err", err);
-                        navigate("/guest/login");
-                    });
-            } catch (err) {
-                navigate("/guest/login");
+    const checkGuestSession = async () => {
+      try {
+        await axios
+          .get(
+            `${
+              import.meta.env.VITE_BASE_ENDPOINTMENT
+            }guests/check-guest-session?eventId=${eventId}&guestId=${guestId}`,
+            {
+              withCredentials: true,
             }
-        };
-
-        const fetchGuestData = async () => {
-            try {
-                await axios
-                    .get(
-                        `${import.meta.env.VITE_BASE_ENDPOINTMENT
-                        }guests/get-guest-data?eventId=${eventId}`,
-                        {
-                            withCredentials: true,
-                        }
-                    )
-                    .then((res) => {
-                        console.log("guest data", res.data.data);
-                        if (res.data.data) {
-                            setGuestData(res.data.data);
-                        } else {
-                            navigate("/guest/login");
-                        }
-                    })
-                    .catch((err) => {
-                        console.log("err", err);
-                        navigate("/guest/login");
-                    });
-            } catch (err) {
-                console.log("err", err);
-                navigate("/guest/login");
+          )
+          .then((res) => {
+            if (res.data.success === false) {
+              navigate("/guest/login");
             }
-        };
+          })
+          .catch((err) => {
+            console.log("err", err);
+            navigate("/guest/login");
+          });
+      } catch (err) {
+        navigate("/guest/login");
+      }
+    };
 
-        if (!guestId) navigate("/guest/login");
-        if (guestId)
-            if (eventId) {
-                checkGuestSession();
-                fetchEventData();
-                fetchThumbnail();
-                fetchGuestData();
+    const fetchGuestData = async () => {
+      try {
+        await axios
+          .get(
+            `${
+              import.meta.env.VITE_BASE_ENDPOINTMENT
+            }guests/get-guest-data?eventId=${eventId}`,
+            {
+              withCredentials: true,
+            }
+          )
+          .then((res) => {
+            console.log("guest data", res.data.data);
+            if (res.data.data) {
+              setGuestData(res.data.data);
             } else {
-                navigate("/guest/login");
+              navigate("/guest/login");
             }
-    }, [eventId, guestId, navigate]);
+          })
+          .catch((err) => {
+            console.log("err", err);
+            navigate("/guest/login");
+          });
+      } catch (err) {
+        console.log("err", err);
+        navigate("/guest/login");
+      }
+    };
 
-    if (!eventId || isLoading) {
-        return <LoadingIndicator />;
-    }
+    if (!guestId) navigate("/guest/login");
+    if (guestId)
+      if (eventId) {
+        checkGuestSession();
+        fetchEventData();
+        fetchThumbnail();
+        fetchGuestData();
+      } else {
+        navigate("/guest/login");
+      }
+  }, [eventId, guestId, navigate]);
 
-    const ModalDescription = () => {
-        const [opened, { open, close }] = useDisclosure(false);
+  if (!eventId || isLoading) {
+    return <LoadingIndicator />;
+  }
 
-        return (
-            <>
-                <Text c="graycolor.2" mb="xs">
-                    Description
-                </Text>
+  return (
+    <body style={{}}>
+      <AspectRatio ratio={320 / 100} maw="100vw">
+        <Image src={thumbnailUrl} height={200} />
+      </AspectRatio>
 
+      <Grid w="100%" style={{ height: "10rem" }} p="xl">
+        <Grid.Col span={12} h="max-content">
+          <Card
+            p="xl"
+            className={styles.cardContainer}
+            style={{ top: "-7rem" }}
+          >
+            <div style={{ marginBottom: "1rem" }}>
+              <Title order={3} c="redcolor.4" fw={500}>
+                {eventData?.event_name}
+              </Title>
+              <Text mt="5" c="graycolor.2">
+                {`${moment(eventData?.start_date).format(
+                  "MMMM Do YY HH:mm A"
+                )} - ${moment(eventData?.end_date).format(
+                  "MMMM Do YY HH:mm A"
+                )}`}{" "}
+                <Badge color="redcolor.4" ml="sm">
+                  {moment(eventData?.end_date).fromNow()}
+                </Badge>
+              </Text>
+            </div>
+
+            <div>
+              <Text fw={500} c="darkcolor.9">
+                Your Virtual Money
+              </Text>
+              <Text size="base" c="redcolor.4">
+                {guestData?.virtual_money.toLocaleString()}{" "}
+                {eventData?.unit_money}
+              </Text>
+            </div>
+
+            <Divider size="xs" mt="md" color="graycolor.2" />
+            <Box mt="1rem">
+              <Group justify="flex-start" mb={5}>
+                <UnstyledButton onClick={toggle}>
+                  <Group align="center">
+                    <Text>Description</Text>
+                    {isOpened ? (
+                      <IconChevronDown size={16} />
+                    ) : (
+                      <IconChevronUp size={16} />
+                    )}
+                  </Group>
+                </UnstyledButton>
+              </Group>
+
+              <Collapse in={isOpened}>
                 <Flex align="flex-end">
-                    <Text lineClamp={5}>
-                        <div
-                            dangerouslySetInnerHTML={{ __html: eventData?.description.toString() || "" }}
-                        />
-                    </Text>
-                    <ActionIcon variant="subtle" onClick={open} color="greencolor.4">
-                        <IconArrowsDiagonal size={14} stroke={1.5} />
-                    </ActionIcon>
+                  <Text
+                    lineClamp={5}
+                    dangerouslySetInnerHTML={{
+                      __html: eventData?.description.toString() || "",
+                    }}
+                  ></Text>
+                  <ActionIcon
+                    variant="subtle"
+                    onClick={open}
+                    color="redcolor.4"
+                  >
+                    <IconArrowsDiagonal size={14} stroke={1.5} />
+                  </ActionIcon>
                 </Flex>
 
                 <Modal
-                    opened={opened}
-                    onClose={close}
-                    title="Description"
-                    centered
-                    radius="xs"
-                    size="90%"
-                    padding="lg"
-                    className={styles.scrollBar}
+                  opened={opened}
+                  onClose={close}
+                  title="Description"
+                  centered
+                  radius="xs"
+                  size="95%"
+                  padding="lg"
+                  className={styles.scrollBar}
                 >
-                    <Text>
-                        <div
-                            dangerouslySetInnerHTML={{ __html: eventData?.description.toString() || "" }}
-                        />
-                    </Text>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: eventData?.description.toString() || "",
+                    }}
+                  ></div>
+                  <Flex mt="md" justify="flex-start">
+                    {eventData?.video_link ? (
+                      <Anchor href={eventData?.video_link} underline="always">
+                        Video Link
+                      </Anchor>
+                    ) : (
+                      ""
+                    )}
+                  </Flex>{" "}
                 </Modal>
-            </>
-        );
-    };
-
-    return (
-        <body>
-            <AspectRatio ratio={970 / 150} maw="100vw">
-                <Image src={thumbnailUrl} height={200} />
-            </AspectRatio>
-
-            <Box w="80%" mx="auto" style={{ position: "relative" }}>
-                <Grid justify="flex-start" align="flex-start" mt="xl" mb="md">
-                    <Grid.Col span={12}>
-                        <div>
-                            <Text size="header" c="greencolor.4" fw={600} mb="xs">
-                                {eventData?.event_name}
-                            </Text>
-                            <Flex gap="2rem">
-                                <div>
-                                    <Text size="xsmall" c="graycolor.2" mb="xs">
-                                        Start of event
-                                    </Text>
-                                    {moment(eventData?.start_date).format("LL [at] HH:mm A")}
-                                </div>
-                                <div>
-                                    <Text size="xsmall" c="graycolor.2" mb="xs">
-                                        End of event
-                                    </Text>
-                                    {moment(eventData?.start_date).format("LL [at] HH:mm A")}
-                                </div>
-                            </Flex>
-
-                            <Badge color="greencolor.4" style={{ position: "absolute", top: "0.25rem", right: "0" }}>
-                                {moment(eventData?.end_date).fromNow()}
-                            </Badge>
-                        </div>
-                    </Grid.Col>
-
-                    <Grid.Col my="md">
-                        <Paper
-                            withBorder
-                            p="md"
-                            radius="md"
-                            bg="none"
-                            h="max-content"
-                        >
-                            <Group justify="space-between">
-                                <Text c="graycolor.2">All virtual money</Text>
-                                <IconCoins size={16} />
-                            </Group>
-
-                            <Group align="flex-end" gap="xs" mt={25}>
-                                <Text fw={500} c="greencolor.4">
-                                    {guestData?.virtual_money.toLocaleString()}{" "}
-                                    {eventData?.unit_money}
-                                </Text>
-                            </Group>
-
-                            <Text fz="xs" c="dimmed" mt={7}>
-                                Your Virtual Money
-                            </Text>
-                        </Paper>
-                    </Grid.Col>
-
-                    <Grid.Col span={12}>
-                        <ModalDescription />
-                    </Grid.Col>
-
-                    <Grid.Col span={12}>
-                        <ProjectsDashboard eventId={eventData?.id.toString()} />
-                    </Grid.Col>
-                </Grid>
+              </Collapse>
             </Box>
-        </body>
-    );
+          </Card>
+        </Grid.Col>
+      </Grid>
+
+      <ProjectsDashboard eventId={eventData?.id.toString()} />
+    </body>
+  );
 }
