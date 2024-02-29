47da010c1048adf91523d1e2969c2d260d6d5819
Panthonf <panthon_kansap@cmu.ac.th>
2024-02-29 18:26:56 +0700

    Update projectsDashboard.tsx

--- src/Guest/projectsDashboard.tsx
+++ src/Guest/projectsDashboard.tsx
@@ -1,5 +1,4 @@
 import {
-  Button,
   Card,
   Center,
   Divider,
@@ -7,13 +6,19 @@ import {
   Pagination,
   Text,
   TextInput,
-  Anchor,
+  Flex,
+  UnstyledButton,
+  Modal,
+  Group,
+  ActionIcon,
 } from "@mantine/core";
-import { IconSearch, IconArrowNarrowRight } from "@tabler/icons-react";
+import { IconSearch, IconCoin } from "@tabler/icons-react";
 import axios from "axios";
 import moment from "moment";
 import { useEffect, useState } from "react";
 import styles from "../styles.module.css";
+import { useDisclosure } from "@mantine/hooks";
+import GuestProject from "./guestProject";
 
 export default function ProjectsDashboard(props: {
   eventId: string | undefined;
@@ -44,7 +49,6 @@ export default function ProjectsDashboard(props: {
         );
         setTotalProjects(response.data.totalProjects);
         setProjectsData(response.data.data);
-        console.log("response.data", response.data.data);
       } catch (err) {
         // console.error("Error fetching events:", error);
       }
@@ -55,97 +59,173 @@ export default function ProjectsDashboard(props: {
     }
   }, [page, props.eventId, query, pageSize]);
 
+  const ModalProjectDetails = ({
+    projectTitle,
+    description,
+  }: {
+    projectTitle: string;
+    description: string;
+  }) => {
+    const [opened, { open, close }] = useDisclosure(false);
+
+    return (
+      <div>
+        <div>
+          <Modal
+            opened={opened}
+            onClose={close}
+            title="Project Details"
+            centered
+          >
+            <Text size="topic" c="greencolor.4" fw={500} truncate="end">
+              {projectTitle}
+            </Text>
+
+            <div style={{ marginTop: "1rem" }}>
+              <Text size="xsmall" mb="xs">
+                Description
+              </Text>
+              <Text>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: description.toString() || "",
+                  }}
+                />
+              </Text>
+            </div>
+          </Modal>
+
+          <Flex align="center" gap="md">
+            <Text size="topic" c="greencolor.4" fw={500} truncate="end">
+              {projectTitle}
+            </Text>
+
+            <UnstyledButton c="graycolor.2" onClick={open}>
+              <Text c="graycolor.2" size="small">
+                Details
+              </Text>
+            </UnstyledButton>
+          </Flex>
+        </div>
+      </div>
+    );
+  };
+
+  const ModalGiveVirtualMoney = ({ projectId }: { projectId: number }) => {
+    const [opened, { open, close }] = useDisclosure(false);
+
+    return (
+      <div>
+        <Modal opened={opened} onClose={close} title="Give Virtual Money">
+          <GuestProject projectId={projectId} />
+        </Modal>
+        <ActionIcon
+          variant="filled"
+          color="greencolor.4"
+          aria-label="Settings"
+          onClick={open}
+        >
+          <IconCoin />
+        </ActionIcon>
+      </div>
+    );
+  };
+
   return (
     <body>
-      <Grid w="100%" p="xl">
-        <Grid.Col span={12}>
-          <Text c="redcolor.4" fw={500} size="topic" mt="md">
-            Projects {projectsData.length > 0 ? `(${totalProjects})` : ""}
-          </Text>
-        </Grid.Col>
-
-        <Grid.Col span={12}>
-          <TextInput
-            value={query}
-            onChange={(project) => setQuery(project.target.value)}
-            placeholder="Search events"
-            rightSection={<IconSearch size={14} />}
-          />
-        </Grid.Col>
-
-        <Grid.Col>
-          {projectsData.length > 0 ? (
-            <div>
-              {projectsData.map((project: ProjectType) => (
-                <Card className={styles.cardContainer} p="1rem" radius="md">
-                  <Grid p={0}>
-                    {/* <Grid.Col span="auto" p={0}>
-                                            {thumbnailUrl && (
-                                                <AspectRatio ratio={1} maw={200} p={0}>
-                                                    <img src={thumbnailUrl} style={{ borderRadius: "0.2rem" }} />
-                                                </AspectRatio>
-                                            )}
-                                        </Grid.Col> */}
-                    <Grid.Col span={12} pl="1rem">
-                      <Text size="topic" c="redcolor.4" fw={500} truncate="end">
-                        {project.title}
-                      </Text>
-
-                      <Text>
-                        {moment(project.created_at).format(
-                          "MMMM Do YY HH:mm a"
-                        )}
-                      </Text>
-
-                      <Divider size="xs" color="graycolor.2" />
-
-                      <div style={{ marginTop: "1rem" }}>
-                        <Text size="xsmall" mb="xs">
-                          Description
-                        </Text>
-                        <Text lineClamp={2}>
-                          <div
-                            dangerouslySetInnerHTML={{
-                              __html: project.description.toString() || "",
-                            }}
-                          ></div>
-                        </Text>
-                      </div>
-                    </Grid.Col>
-
-                    <Grid.Col span={12} ta="end">
-                      <Anchor
-                        href={`/guest/event/${props.eventId}/project/${project.id}`}
-                        underline="never"
-                        ta="end"
-                      >
-                        <Button
-                          rightSection={<IconArrowNarrowRight size={14} />}
-                          size="small"
-                        >
-                          View project
-                        </Button>
-                      </Anchor>
-                    </Grid.Col>
-                  </Grid>
-                </Card>
-              ))}
-            </div>
-          ) : (
-            <Text mt="md">No projects found</Text>
-          )}
-
-          <Center mt="md">
-            <Pagination
-              total={Math.ceil(totalProjects / pageSize)}
-              boundaries={2}
-              value={page}
-              onChange={(newPage) => setPage(newPage)}
-              color="redcolor.4"
+      <div>
+        <Grid w="100%">
+          <Grid.Col span={12}>
+            <Text c="greencolor.4" fw={500} size="topic" mt="md">
+              Projects {projectsData.length > 0 ? `(${totalProjects})` : ""}
+            </Text>
+          </Grid.Col>
+          <Grid.Col span={12} mb="md">
+            <TextInput
+              value={query}
+              onChange={(project) => setQuery(project.target.value)}
+              placeholder="Search events"
+              rightSection={<IconSearch size={14} />}
             />
-          </Center>
-        </Grid.Col>
-      </Grid>
+          </Grid.Col>
+          <Grid.Col>
+            {projectsData.length > 0 ? (
+              <div>
+                {projectsData.map((project: ProjectType) => (
+                  <Card
+                    className={styles.cardContainer}
+                    p="1rem"
+                    radius="md"
+                    mb="0.3rem"
+                  >
+                    <Grid p={0} style={{ position: "relative" }}>
+                      {/* <Grid.Col span="auto" p={0}>
+                                                {thumbnailUrl && (
+                                                    <AspectRatio ratio={1} maw={200} p={0}>
+                                                        <img src={thumbnailUrl} style={{ borderRadius: "0.2rem" }} />
+                                                    </AspectRatio>
+                                                )}
+                                            </Grid.Col> */}
+                      <Grid.Col pl="1rem">
+                        <ModalProjectDetails
+                          projectTitle={project.title as string}
+                          description={project.description as string}
+                        />
+                        <Text my="xs" c="graycolor.2" size="small">
+                          {moment(project.created_at).format(
+                            "MMMM Do YY HH:mm A"
+                          )}
+                        </Text>
+                        <Divider size="xs" color="graycolor.2" />
+                        <div style={{ marginTop: "1rem" }}>
+                          <Text size="xsmall" mb="xs">
+                            Description
+                          </Text>
+                          <Text lineClamp={2}>
+                            <div
+                              dangerouslySetInnerHTML={{
+                                __html: project.description.toString() || "",
+                              }}
+                            />
+                          </Text>
+                        </div>
+                      </Grid.Col>
+                      <div
+                        style={{ position: "absolute", top: "0", right: "0" }}
+                      >
+                        <ModalGiveVirtualMoney projectId={Number(project.id)} />
+                      </div>
+                    </Grid>
+                  </Card>
+                ))}
+              </div>
+            ) : (
+              <Text mt="md">No projects found</Text>
+            )}
+            <Center mt="md">
+              <Pagination.Root
+                color="greencolor.4"
+                size="sm"
+                total={Math.ceil(totalProjects / pageSize)}
+                boundaries={2}
+                value={page}
+                onChange={(newPage) => setPage(newPage)}
+              >
+                <Group gap={5} justify="center">
+                  <Pagination.First />
+                  <Pagination.Previous />
+                  <Pagination.Items />
+                  <Pagination.Next />
+                  <Pagination.Last />
+                </Group>
+              </Pagination.Root>
+            </Center>
+          </Grid.Col>
+        </Grid>
+
+        {/* footer */}
+        {/* <Affix mt="2rem" className={`${styles.footer} ${styles.guest}`}></Affix> */}
+      </div>
     </body>
   );
 }
