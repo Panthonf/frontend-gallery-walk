3b661f889eb1e3b8d7fc4eb66a9e7d5b8a00fadb
Panthonf <panthon_kansap@cmu.ac.th>
2024-03-31 19:12:45 +0700

    Update eventSummary.tsx

--- src/Event Manager and Presenter/eventSummary.tsx
+++ src/Event Manager and Presenter/eventSummary.tsx
@@ -1,4 +1,4 @@
-import { Card, Divider, Flex, Grid, Text, Title } from "@mantine/core";
+import { Card, Divider, Flex, Grid, Select, Text, Title } from "@mantine/core";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
@@ -8,11 +8,24 @@ export default function EventSummary(props: { eventId: React.ReactNode }) {
     totalGiveVirtualMoney: number;
     totalGiveComments: number;
     totalVirtualMoney: number;
+    totalComments: number;
+    projectsRanking: {
+      id: number;
+      title: string;
+      amount: number;
+    }[];
+    projectsNotRanked: {
+      id: number;
+      title: string;
+      amount: number;
+    }[];
     event: {
-        id: number;
-        unit_money: string;
-    }
+      id: number;
+      unit_money: string;
+    };
   } | null>(null);
+
+  const [selectedProject, setSelectedProject] = useState("5");
 
   useEffect(() => {
     const fetchProjectResult = async () => {
@@ -39,6 +52,7 @@ export default function EventSummary(props: { eventId: React.ReactNode }) {
       <Title order={1} c="red">
         Event Summary
       </Title>
+
       <Grid mt="lg" style={{ display: "flex" }}>
         <Grid.Col span={6}>
           {/* Column 1 */}
@@ -70,38 +84,38 @@ export default function EventSummary(props: { eventId: React.ReactNode }) {
         </Grid.Col>
 
         <Grid.Col span={6}>
-            {/* Column 1 */}
-            <Card shadow="xs" padding="md" radius="md">
-              <Flex align={"baseline"} justify={"flex-start"}>
-                <Text size="xl" c={"gray"} fw="bold">
-                  Total Virtual Money
+          {/* Column 1 */}
+          <Card shadow="xs" padding="md" radius="md">
+            <Flex align={"baseline"} justify={"flex-start"}>
+              <Text size="xl" c={"gray"} fw="bold">
+                Total Virtual Money
+              </Text>
+              <Text ml="sm" size="xs" c={"gray"} fw="bold">
+                (จำนวน Virtual Money ที่ทุกโครงการได้รับ)
+              </Text>
+            </Flex>
+            <Divider />
+            {summary ? (
+              <Flex align={"baseline"} justify={"flex-start"} mt="3">
+                <Text style={{ fontSize: "3rem" }} c="red">
+                  {summary.totalVirtualMoney.toLocaleString()}
                 </Text>
-                <Text ml="sm" size="xs" c={"gray"} fw="bold">
-                  (จำนวน Virtual Money ที่ทุกโครงการได้รับ)
+                <Text ml="md" fz={"sm"} fw={"unset"}>
+                  {summary.event.unit_money}
                 </Text>
               </Flex>
-              <Divider />
-              {summary ? (
-                <Flex align={"baseline"} justify={"flex-start"} mt="3">
-                  <Text style={{ fontSize: "3rem" }} c="red">
-                    {summary.totalVirtualMoney.toLocaleString()}
-                  </Text>
-                  <Text ml="md" fz={"sm"} fw={"unset"}>
-                    {summary.event.unit_money}
-                  </Text>
-                </Flex>
-              ) : (
-                <Text mt="3" style={{ fontSize: "3rem" }}>
-                  <Text>Loading...</Text>
-                </Text>
-              )}
-            </Card>
+            ) : (
+              <Text mt="3" style={{ fontSize: "3rem" }}>
+                <Text>Loading...</Text>
+              </Text>
+            )}
+          </Card>
         </Grid.Col>
       </Grid>
 
-      <Grid mt="lg" style={{ display: "flex" }}>
-        <Grid.Col span={6}>
-        <Card shadow="xs" padding="md" radius="md">
+      <Grid mt="lg">
+        <Grid.Col span={6} h="100%">
+          <Card shadow="xs" padding="md" radius="md">
             <Flex align={"baseline"} justify={"flex-start"}>
               <Text size="xl" c={"gray"} fw="bold">
                 Guest Contributors
@@ -112,7 +126,7 @@ export default function EventSummary(props: { eventId: React.ReactNode }) {
             </Flex>
 
             <Divider />
-            <Grid mt="lg" style={{ display: "flex" }}>
+            <Grid mt="lg" h="100%">
               <Grid.Col span={6}>
                 <Card shadow="xs" padding="md" radius="md">
                   <Text>Give Virtual Money</Text>
@@ -154,6 +168,121 @@ export default function EventSummary(props: { eventId: React.ReactNode }) {
                 </Card>
               </Grid.Col>
             </Grid>
+          </Card>
+        </Grid.Col>
+
+        <Grid.Col span={6} h="100%">
+          <Card shadow="xs" padding="md" radius="md">
+            <Flex align={"baseline"} justify={"flex-start"}>
+              <Text size="xl" c={"gray"} fw="bold">
+                Total Comments
+              </Text>
+              <Text ml="sm" size="xs" c={"gray"} fw="bold">
+                (จำนวนความคิดเห็นทั้งหมด)
+              </Text>
+            </Flex>
+
+            <Divider />
+            <Grid mt="lg" h="100%">
+              <Grid.Col span={6}>
+                {summary ? (
+                  <Flex align={"baseline"} justify={"flex-start"} mt="3">
+                    <Text style={{ fontSize: "3rem" }} c="red">
+                      {summary.totalComments}
+                    </Text>
+                    <Text ml="md" fz={"sm"} fw={"unset"}>
+                      comments
+                    </Text>
+                  </Flex>
+                ) : (
+                  <Text mt="3" style={{ fontSize: "3rem" }}>
+                    <Text>Loading...</Text>
+                  </Text>
+                )}
+              </Grid.Col>
+            </Grid>
+          </Card>
+        </Grid.Col>
+      </Grid>
+
+      <Grid mt="lg" style={{ display: "flex" }}>
+        <Grid.Col span={12}>
+          <Card shadow="xs" padding="md" radius="md">
+            <Text size="xl" c={"gray"} fw="bold">
+              Projects Ranking
+            </Text>
+            <Divider />
+            <Select
+              //   label="Select number of projects to show"
+              placeholder="Select number of projects to show"
+              data={["5", "10", "15", "All"]}
+              mt="md"
+              defaultValue={"5"}
+              onChange={(value) => {
+                setSelectedProject(value || "");
+              }}
+            />
+            {summary ? (
+              <>
+                {summary.projectsRanking
+                  .slice(
+                    0,
+                    selectedProject === "All"
+                      ? summary.projectsRanking.length
+                      : parseInt(selectedProject)
+                  )
+                  .map((project, index) => (
+                    <Flex
+                      align={"baseline"}
+                      justify={"space-between"}
+                      key={project.id}
+                    >
+                      <Text size="md" c={"gray"} fw="400">
+                        {index + 1}. {project.title}
+                      </Text>
+                      <Flex align={"baseline"} justify={"flex-end"}>
+                        <Text fw="bold" size="lg" c={"gray"}>
+                          {project.amount.toLocaleString()}{" "}
+                        </Text>
+                        <Text ml="md" size="md" c={"gray"} fw="400">
+                          {summary.event.unit_money}
+                        </Text>
+                      </Flex>
+                    </Flex>
+                  ))}
+              </>
+            ) : (
+              <Text>Loading...</Text>
+            )}
+          </Card>
+        </Grid.Col>
+      </Grid>
+      <Grid mt="lg" style={{ display: "flex" }}>
+        <Grid.Col span={12}>
+          <Card shadow="xs" padding="md" radius="md">
+            <Text size="xl" c={"gray"} fw="bold">
+              Projects Not Ranked
+            </Text>
+            <Divider />
+
+            {summary ? (
+              <>
+                {summary.projectsNotRanked.map((project, index) => (
+                  <Flex
+                    mt="md"
+                    align={"baseline"}
+                    justify={"space-between"}
+                    key={project.id}
+                  >
+                    <Text size="md" c={"gray"} fw="400">
+                      {index + 1}. {project.title}
+                    </Text>
+                  </Flex>
+                ))}
+              </>
+            ) : (
+              <Text>Loading...</Text>
+            )}
           </Card>
         </Grid.Col>
       </Grid>
